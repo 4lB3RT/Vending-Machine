8bd9621285f09511c578f982f4a1dbdb06a7d6b3
albert garcia <albertgarcia.agc@gmail.com>
2025-11-15 23:34:18 +0100

    add productTest

---
+++ tests/Unit/Product/Domain/Entities/ProductTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Tests\Unit\Product\Domain\Entities;
+
+use PHPUnit\Framework\TestCase;
+
+final class ProductTest extends TestCase
+{
+    public function testProductIsCreated(): void
+    {
+        $uuid     = '123e4567-e89b-12d3-a456-426614174000';
+        $name     = 'Coke';
+        $price    = 1.5;
+        $quantity = 10;
+        $product  = ProductMother::create($uuid, $name, $price, $quantity);
+
+        $this->assertEquals($uuid, $product->id()->value());
+        $this->assertEquals($name, $product->name()->value());
+        $this->assertEquals($price, $product->price()->value());
+        $this->assertEquals($quantity, $product->quantity()->value());
+    }
+}
