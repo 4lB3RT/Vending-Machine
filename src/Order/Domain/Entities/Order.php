bb5dd0f068765317a44bb5a97b9534f0bba9dae6
albert garcia <albertgarcia.agc@gmail.com>
2025-11-19 19:15:10 +0100

    Refactor Order entities, remove OrderCoins, and update related tests

---
+++ src/Order/Domain/Entities/Order.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace VendingMachine\Order\Domain\Entities;
+
+use VendingMachine\Order\ValueObjects\OrderId;
+use VendingMachine\Product\Domain\Collections\ProductCollection;
+use VendingMachine\Product\Domain\Entities\Product;
+use VendingMachine\Wallet\Domain\Entities\Wallet;
+
+final readonly class Order
+{
+    public function __construct(
+        private OrderId $id,
+        private ProductCollection $products,
+        private Wallet $wallet,
+    ) {
+    }
+
+    public function id(): OrderId
+    {
+        return $this->id;
+    }
+
+    public function wallet(): Wallet
+    {
+        return $this->wallet;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id'       => $this->id->value(),
+            'products' => $this->products()->map(fn (Product $product) => $product->toArray()),
+            'wallet'   => $this->wallet->toArray(),
+        ];
+    }
+
+    public function products(): ProductCollection
+    {
+        return $this->products;
+    }
+}
--- tests/Unit/Order/Domain/Entities/OrderMother.php
+++ tests/Unit/Order/Domain/Entities/OrderMother.php
@@ -4,12 +4,10 @@ declare(strict_types = 1);
 
 namespace Tests\Unit\Order\Domain\Entities;
 
-use Tests\Unit\Order\Domain\ValueObjects\OrderCoinsMother;
 use Tests\Unit\Order\Domain\ValueObjects\OrderIdMother;
 use Tests\Unit\Product\Domain\Entities\ProductMother;
 use Tests\Unit\Wallet\Domain\Entities\WalletMother;
-use VendingMachine\Order\Entities\Order;
-use VendingMachine\Order\ValueObjects\OrderCoins;
+use VendingMachine\Order\Domain\Entities\Order;
 use VendingMachine\Order\ValueObjects\OrderId;
 use VendingMachine\Product\Domain\Collections\ProductCollection;
 use VendingMachine\Wallet\Domain\Entities\Wallet;
@@ -20,7 +18,6 @@ final class OrderMother
         ?OrderId $id = null,
         ?ProductCollection $products = null,
         ?Wallet $wallet = null,
-        ?OrderCoins $coins = null
     ): Order {
         $productOne = ProductMother::create();
         $productTwo = ProductMother::create();
@@ -31,7 +28,6 @@ final class OrderMother
             $id ?? OrderIdMother::create(),
             $products,
             $wallet ?? WalletMother::create(),
-            $coins  ?? OrderCoinsMother::create()
         );
     }
 }
