adfe2c6bbaec049071d5c68a0fbe68737631bb54
albert garcia <albertgarcia.agc@gmail.com>
2025-11-20 18:14:44 +0100

    add createOrderControll with test and and route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
+use VendingMachine\Order\Infrastructure\Http\Api\Controllers\CreateOrderController;
 use VendingMachine\Product\Infrastructure\Http\Api\Controllers\GetProductsController;
 use VendingMachine\Wallet\Infrastructure\Http\Api\Controllers\CreateWalletController;
 use VendingMachine\Wallet\Infrastructure\Http\Api\Controllers\GetWalletController;
@@ -18,5 +19,11 @@ Route::group(['prefix' => 'wallets'], function () {
         Route::get('/', [GetWalletController::class, '__invoke']);
         Route::put('/', [UpdateWalletController::class, '__invoke']);
         Route::post('/', [CreateWalletController::class, '__invoke']);
+    });
+});
+
+Route::group(['prefix' => 'orders'], function () {
+    Route::group(['prefix' => '{uuid}'], function () {
+        Route::post('/', [CreateOrderController::class, '__invoke']);
     });
 });
