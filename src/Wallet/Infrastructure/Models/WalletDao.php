a9888301129d7d4b208fb43cfb130024c030c9f5
albert garcia <albertgarcia.agc@gmail.com>
2025-11-20 19:09:30 +0100

    add EloquentWalletRepository with test and WalletDao

---
+++ src/Wallet/Infrastructure/Models/WalletDao.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace VendingMachine\Wallet\Infrastructure\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Database\Factories\WalletDaoFactory;
+
+/**
+ * @property string $id
+ * @property string $name
+ * @property float  $coins
+ */
+final class WalletDao extends Model
+{
+    public $incrementing  = false;
+    protected $table      = 'wallets';
+    protected $primaryKey = 'id';
+    protected $keyType    = 'string';
+    protected $fillable   = [
+        'id', 'name', 'coins',
+    ];
+
+    public static function factory(): WalletDaoFactory
+    {
+        return new WalletDaoFactory();
+    }
+}
